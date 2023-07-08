8fee7c8469ecfb845affe354a989c80a8f63a826
TyomoGit <riku3dno.1@gmail.com>
2023-07-08 23:50:55 +0900

    infoファイルを追加

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::fs;
+use std::io::Write;
+use chrono;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 1 {
         make_python_files(&String::from("."));
+        make_info_file(&String::from("."));
         return;
     }
 
@@ -15,6 +18,7 @@ fn main() {
         let dir_name = &arg.to_owned();
         make_directory(&dir_name);
         make_python_files(&dir_name);
+        make_info_file(dir_name);
     }
 
     println!("Done");
@@ -40,9 +44,24 @@ fn make_python_files(dir_name: &String) {
             Err(_) => false,
         };
 
-        if file_exists == false {
-            println!("creating file: {}", &name);
-            fs::File::create(&name).unwrap();
-        }
+        if file_exists == true{ continue; }
+
+        println!("creating file: {}", &name);
+        fs::File::create(&name).unwrap();
     }
+}
+
+fn make_info_file(dir_name: &String) {
+    let name = dir_name.to_owned() + "/" + "info.txt";
+    let file_exists = match fs::metadata(&name) {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
+    if file_exists == true { return; }
+
+    println!("creating file: {}", &name);
+    let mut file = fs::File::create(&name).unwrap();
+    let now = chrono::Local::now().format("%Y/%m/%d %H:%M").to_string();
+    write!(file, "Created at {}", now).unwrap();
 }
